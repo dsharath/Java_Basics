37a6a7117cd1204394af01061a0ade95800f567f
SHARATH CHANDRA <30965381+dsharath@users.noreply.github.com>
2019-07-30 09:07:44 -0400

    Java: Overriding the VarArgs method

---
+++ OverridingVarArgs.java
@@ -0,0 +1,15 @@
+
+public class OverridingVarArgs {
+
+
+	
+	public void m(int... x) {
+		System.out.println("parent");
+	}
+	
+	
+	 public void n(int... x) {
+			
+			System.out.println("parent- overrinding");
+		}
+}
